03080805f7f11e4dac3fe3e719045eca7e7bcaa3
Dewald928 <dewald123@rocketmail.com>
2021-10-14 10:07:25 +0200

    resource tabs

--- src/pages/Resources.vue
+++ src/pages/Resources.vue
@@ -1,19 +1,41 @@
 <template>
   <q-page>
-    <q-card>
-      Hi
-    </q-card>
-    <buysellram></buysellram>
-    <staking></staking>
+    <div class="q-gutter-y-md" style="max-width: 600px">
+      <q-card>
+        <q-tabs
+          v-model="tab"
+        >
+          <q-tab name="ram" label="Buy/Sell RAM" />
+          <q-tab name="netcpu" label="Stake CPU/NET" />
+        </q-tabs>
+
+        <q-separator />
+
+        <q-tab-panels v-model="tab" animated>
+          <q-tab-panel name="ram">
+            <buysellram></buysellram>
+          </q-tab-panel>
+
+          <q-tab-panel name="netcpu">
+            <staking></staking>
+          </q-tab-panel>
+        </q-tab-panels>
+      </q-card>
+    </div>
   </q-page>
 </template>
 
 <script>
-import buysellram from './components/resources/buysellram.vue'
-import Staking from './components/resources/staking.vue'
+import buysellram from "./components/resources/buysellram.vue";
+import Staking from "./components/resources/staking.vue";
 
 export default {
   components: { buysellram, Staking },
   // name: 'PageName',
-}
+  data() {
+    return {
+      tab: "mails"
+    };
+  }
+};
 </script>
