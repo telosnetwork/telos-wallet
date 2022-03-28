143fdda6ec79dc5c8da1dc56dc59846ca7df45e5
Dewald928 <dewald123@rocketmail.com>
2022-03-28 14:39:41 +0200

    Terms of service

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,14 +1,34 @@
 <template>
   <q-page class="column justify-center items-center content-center">
-    <div class="text-center q-pb-md text-h1">
-      Telos Wallet
-    </div>
-    <div class="text-h5 text-center q-pb-lg">
-      It's Crypto Made Easy
-    </div>
+    <div class="text-center q-pb-md text-h1">Telos Wallet</div>
+    <div class="text-h5 text-center q-pb-lg">It's Crypto Made Easy</div>
     <div v-if="!isAuthenticated" class="text-center">
       <login-button />
     </div>
+    <q-footer bordered>
+      <q-toolbar class="bg-dark">
+        <div class="fit row wrap justify-between items-center content-between">
+          <div>
+            <a
+              href="https://www.telos.net/terms-of-service"
+              target="_blank"
+              class="text-white"
+            >
+              Terms of Service
+            </a>
+          </div>
+          <div>
+            <a
+              href="https://www.telos.net/privacy-policy"
+              target="_blank"
+              class="text-white"
+            >
+              Privacy Policy
+            </a>
+          </div>
+        </div>
+      </q-toolbar>
+    </q-footer>
   </q-page>
 </template>
 
@@ -20,15 +40,17 @@ export default {
   name: "Home",
   data() {
     return {
-      slide: "slide1"
+      slide: "slide1",
     };
   },
   components: { LoginButton },
   computed: {
-    ...mapGetters("account", ["isAuthenticated"])
-  }
+    ...mapGetters("account", ["isAuthenticated"]),
+  },
 };
 </script>
 
-<style scoped> 
+<style scoped>
+a {
+}
 </style>
