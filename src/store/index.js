999735e34cdd64cff9785d5325d054eca17145c4
Don Peat <don@caleos.io>
2022-08-31 23:09:53 -0500

    fix: update store

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,10 @@
-import Vue from "vue";
-import Vuex from "vuex";
-
+import { store } from 'quasar/wrappers';
+import { createStore } from 'vuex';
+import general from './general';
+import evm from './evm';
 import account from "./account";
-import general from "./general";
 import global from "./global";
 import resources from "./resources";
-import evm from "./evm";
 import rex from "./rex";
 import { GeneralModule } from "./modules/general";
 import { EosTransitModule } from "./modules/wallet/tlosWallet";
@@ -16,26 +15,27 @@ import { NetworkModule } from "./modules/network/index";
 import { TlosNetworkModule } from "./modules/network/tlosNetwork";
 import { createProxy, extractVuexModule } from "vuex-class-component";
 
-Vue.use(Vuex);
+export default store(function (/* { ssrContext } */) {
+  const Store = createStore({
+      modules: {
+        general,
+        account,
+        global,
+        resources,
+        evm,
+        rex
+      },
 
-export default function() {
-  const Store = new Vuex.Store({
-    modules: {
-      general,
-      account,
-      global,
-      resources,
-      evm,
-      rex
-    },
-
-    strict: process.env.DEV
+      // enable strict mode (adds overhead!)
+      // for dev mode only
+      strict: process.env.DEV,
   });
 
   return Store;
-}
+});
 
-export const store = new Vuex.Store({
+// TODO: refactor this
+export const store = createStore({
   modules: Object.assign(
     Object.assign(
       Object.assign(
@@ -67,3 +67,9 @@ export const vxm = {
   tlosNetwork: createProxy(store, TlosNetworkModule),
   network: createProxy(store, NetworkModule)
 };
+
+
+
+
+
+
