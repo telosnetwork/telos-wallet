f0db12b10f5c77a55eae5b3e1cccf32eac6b7bcd
Don Peat <don@caleos.io>
2022-08-31 21:45:35 -0500

    refactor: remove deprecated authenticators

--- src/boot/ual.js
+++ src/boot/ual.js
@@ -1,7 +1,4 @@
 import { UAL } from "universal-authenticator-library";
-import { KeycatAuthenticator } from "@telosnetwork/ual-telos-keycat";
-import { Scatter } from "ual-scatter";
-import { Sqrl } from "@smontero/ual-sqrl";
 import { Anchor } from "ual-anchor";
 import { Wombat } from "ual-wombat";
 
@@ -18,11 +15,8 @@ export default async ({ Vue, store }) => {
   };
 
   const authenticators = [
-    // new KeycatAuthenticator([chain], { appName: process.env.APP_NAME }),
-    // new Sqrl([chain], { appName: process.env.APP_NAME }),
     new Wombat([chain], { appName: process.env.APP_NAME }),
-    new Anchor([chain], { appName: process.env.APP_NAME }),
-    new Scatter([chain], { appName: process.env.APP_NAME })
+    new Anchor([chain], { appName: process.env.APP_NAME })
   ];
 
   const ual = new UAL([chain], "ual", authenticators);
