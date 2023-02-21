cd4cac8a28bd5df23fbea77d6e4f65d271af4aed
Don Peat <don@caleos.io>
2023-02-21 14:19:29 -0600

    doc: comment conditional for sort coin

--- src/pages/Balance.vue
+++ src/pages/Balance.vue
@@ -549,7 +549,9 @@ export default {
           const aContract = a.account.toLowerCase();
           const bContract = b.account.toLowerCase();
 
+          // both native and evm have same symbol and account name
           if ((aSymbol === bSymbol) && (aContract === bContract)) {
+            // if evm
             if (a.network) {
               return 1;
             } else {
