18ad297952d493547e31a35c2dc6455965921bc5
Ezra Sowden-Guzman <ezratic@gmail.com>
2023-04-03 19:39:31 -0400

    fix: query param routing

--- src/components/evm/AppPage.vue
+++ src/components/evm/AppPage.vue
@@ -15,19 +15,24 @@ export default defineComponent({
             return Array.isArray(this.tabs);
         },
         selectedTab() {
-            return (this.$route.query.tab ?? this.tabs[0] ?? '') as string;
+            return (this.$route.query.tab ?? '') as string;
         },
     },
     watch: {
-        '$route.query.tab': {
+        $route: {
             immediate: true,
-            handler(newValue) {
-                if (this.tabs === null) {
-                    return;
-                }
+            deep: true,
+            handler(newValue, oldValue = null) {
+                const isSameRoute = oldValue === null || newValue?.path === oldValue?.path;
 
-                if (!this.tabs.includes(newValue)) {
-                    this.$router.replace({ query: { tab: this.tabs[0] as string } });
+                if (isSameRoute) {
+                    if (this.tabs === null) {
+                        return;
+                    }
+
+                    if (!this.tabs.includes(newValue.query.tab)) {
+                        this.$router.replace({ query: { tab: this.tabs[0] } });
+                    }
                 }
             },
         },
@@ -44,14 +49,14 @@ export default defineComponent({
 
         <q-tabs
             v-if="showTabs"
-            v-model="selectedTab"
+            :model-value="selectedTab"
             inline-label
             no-caps
             indicator-color="primary"
             class="c-app-page__tabs"
         >
             <q-route-tab
-                v-for="tab in tabs"
+                v-for="tab in (tabs ?? [])"
                 :key="tab"
                 :name="tab"
                 :label="tab.charAt(0).toUpperCase() + tab.slice(1)"
