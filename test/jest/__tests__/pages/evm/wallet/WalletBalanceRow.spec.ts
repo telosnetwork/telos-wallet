52d3af961e1f0284a744dd0340d8e682e21c23ee
Ezra Sowden-Guzman <ezratic@gmail.com>
2023-04-07 18:35:49 -0400

    feat: add waletbalancerow tests

--- test/jest/__tests__/pages/evm/wallet/WalletBalanceRow.spec.ts
+++ test/jest/__tests__/pages/evm/wallet/WalletBalanceRow.spec.ts
@@ -1,15 +1,15 @@
 import { shallowMount } from '@vue/test-utils';
 
 const fakeBuyMoreLink = 'fake';
-const stlosContractAddressMock = jest.fn();
-const wtlosContractAddressMock = jest.fn();
+const fakeStlosContractAddress = '0x'.concat('9'.repeat(40));
+const fakeWtlosContractAddress = '0x'.concat('8'.repeat(40));
 const chainStoreMock = {
     useChainStore: () => ({
         currentChain: {
             settings: {
                 getBuyMoreOfTokenLink: () => fakeBuyMoreLink,
-                getStlosContractAddress: stlosContractAddressMock,
-                getWtlosContractAddress: wtlosContractAddressMock,
+                getStlosContractAddress: () => fakeStlosContractAddress,
+                getWtlosContractAddress: () => fakeWtlosContractAddress,
             },
         },
     }),
@@ -46,7 +46,7 @@ describe('WalletBalanceRow.vue', () => {
             },
             props: {
                 token: {
-                    address: stlosContractAddressMock(),
+                    address: fakeStlosContractAddress,
                     symbol: 'STLOS',
                     name: 'Staked TLOS',
                     logoURI: 'https://raw.githubusercontent.com/telosnetwork/teloscan/master/public/stlos-logo.png',
@@ -75,7 +75,7 @@ describe('WalletBalanceRow.vue', () => {
             },
             props: {
                 token: {
-                    address: stlosContractAddressMock(),
+                    address: fakeStlosContractAddress,
                     symbol: 'STLOS',
                     name: 'Staked TLOS',
                     logoURI: 'https://raw.githubusercontent.com/telosnetwork/teloscan/master/public/stlos-logo.png',
@@ -90,25 +90,127 @@ describe('WalletBalanceRow.vue', () => {
 
     describe('should render the right overflow items when the token is', () => {
         test('TLOS', () => {
+            const wrapper = shallowMount(WalletBalanceRow, {
+                global: {
+                    stubs,
+                    mocks: {
+                        $q: {
+                            screen: {
+                                lt: {
+                                    lg: true,
+                                },
+                            },
+                        },
+                    },
+                },
+                props: {
+                    token: {
+                        symbol: 'TLOS',
+                        name: 'Telos',
+                        logoURI: 'https://raw.githubusercontent.com/telosnetwork/teloscan/master/public/stlos-logo.png',
+                        decimals: 18,
+                        balance: '3642.0243',
+                        fullBalance: '3642.024318091460206147',
+                    },
+                    tokenIsTlos: true,
+                },
+            });
 
+            // snapshot contains c-wallet-balance-row__overflow-li for stake, buy, wrap, send
+            expect(wrapper.element).toMatchSnapshot();
         });
 
         test('STLOS', () => {
+            const wrapper = shallowMount(WalletBalanceRow, {
+                global: {
+                    stubs,
+                    mocks: {
+                        $q: {
+                            screen: {
+                                lt: {
+                                    lg: true,
+                                },
+                            },
+                        },
+                    },
+                },
+                props: {
+                    token: {
+                        address: fakeStlosContractAddress,
+                        symbol: 'STLOS',
+                        name: 'Staked TLOS',
+                        logoURI: 'https://raw.githubusercontent.com/telosnetwork/teloscan/master/public/stlos-logo.png',
+                        decimals: 18,
+                        balance: '3642.0243',
+                        fullBalance: '3642.024318091460206147',
+                    },
+                },
+            });
 
+            // snapshot contains c-wallet-balance-row__overflow-li for stake, contract, send
+            expect(wrapper.element).toMatchSnapshot();
         });
 
         test('WTLOS', () => {
+            const wrapper = shallowMount(WalletBalanceRow, {
+                global: {
+                    stubs,
+                    mocks: {
+                        $q: {
+                            screen: {
+                                lt: {
+                                    lg: true,
+                                },
+                            },
+                        },
+                    },
+                },
+                props: {
+                    token: {
+                        address: fakeWtlosContractAddress,
+                        symbol: 'WTLOS',
+                        name: 'Wrapped TLOS',
+                        logoURI: 'https://raw.githubusercontent.com/telosnetwork/teloscan/master/public/stlos-logo.png',
+                        decimals: 18,
+                        balance: '3642.0243',
+                        fullBalance: '3642.024318091460206147',
+                    },
+                },
+            });
 
+            // snapshot contains c-wallet-balance-row__overflow-li for unwrap, contract, send
+            expect(wrapper.element).toMatchSnapshot();
         });
 
-        describe('not a system token and', () => {
-            it('has a reliable fiat value', () => {
-
+        test('not a system token', () => {
+            const wrapper = shallowMount(WalletBalanceRow, {
+                global: {
+                    stubs,
+                    mocks: {
+                        $q: {
+                            screen: {
+                                lt: {
+                                    lg: true,
+                                },
+                            },
+                        },
+                    },
+                },
+                props: {
+                    token: {
+                        address: '0x'.concat('1'.repeat(40)),
+                        symbol: 'SHIB',
+                        name: 'Shiba',
+                        logoURI: 'https://raw.githubusercontent.com/telosnetwork/teloscan/master/public/stlos-logo.png',
+                        decimals: 18,
+                        balance: '3642.0243',
+                        fullBalance: '3642.024318091460206147',
+                    },
+                },
             });
 
-            it('does not have a reliable fiat value', () => {
-
-            });
+            // snapshot contains c-wallet-balance-row__overflow-li for contract, send
+            expect(wrapper.element).toMatchSnapshot();
         });
     });
 });
