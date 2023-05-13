1d419428db9c6f49988c936109bf2ad5d952bd3a
Don Peat <don@caleos.io>
2023-05-13 15:50:50 -0500

    fix: update rpc endpoint

--- src/api/rpc.js
+++ src/api/rpc.js
@@ -1,3 +1,3 @@
 import { JsonRpc } from "eosjs";
-export const rpc = new JsonRpc("https://telos.caleos.io");
+export const rpc = new JsonRpc("https://mainnet.telos.net");
 export const xrpc = new JsonRpc("https://eos.greymass.com");
