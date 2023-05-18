ad021e21bf0597cfe528c2ea144cf6ac85151086
Ezra Sowden-Guzman <ezratic@gmail.com>
2023-05-18 18:37:54 -0400

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -55,7 +55,6 @@ module.exports = {
 
         '@typescript-eslint/explicit-function-return-type': 'off',
         '@typescript-eslint/explicit-module-boundary-types': 'off',
-        '@typescript-eslint/no-non-null-assertion' : 'off',
 
         // allow debugger during development only
         'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
