5fdc2b575f43e8fc86364d2377f090e59d30bca2
Ezra Sowden-Guzman <ezratic@gmail.com>
2023-05-26 20:27:41 -0400

    add to metamask functionality

--- src/antelope/stores/utils/currency-utils.ts
+++ src/antelope/stores/utils/currency-utils.ts
@@ -312,3 +312,46 @@ export function getCurrencySymbol(locale: string, currencyCode: string) {
 
     return symbol;
 }
+
+/**
+ * Launches a prompt in MetaMask to add a given token as a tracked token, allowing the user to view their balance of
+ * that token at a glance from MetaMask
+ *
+ * @param {string}    address - the address of the token contract
+ * @param {string}    symbol - the token's ticker symbol, e.g. 'STLOS'
+ * @param {string}    image - permalink url of the token's icon
+ * @param {string}    type - Ethereum standard of the token; default is 'ERC20'
+ * @param {number}    decimals - the number of decimals constituting the token's precision, default is 18
+ *
+ * @returns {Promise<void>}
+ */
+export async function promptAddToMetamask(
+    address: string,
+    symbol: string,
+    image: string,
+    type: string,
+    decimals: number,
+): Promise<void> {
+    if (!window.ethereum) {
+        return Promise.reject();
+    }
+
+    type MetamaskEthereum = {
+        request: (args: { method: string, params: Record<string, unknown> }) => Promise<void>
+    };
+
+    const ethereum = window.ethereum as unknown as MetamaskEthereum;
+
+    return ethereum.request({
+        method: 'wallet_watchAsset',
+        params: {
+            type,
+            options: {
+                address,
+                symbol,
+                decimals,
+                image,
+            },
+        },
+    });
+}
