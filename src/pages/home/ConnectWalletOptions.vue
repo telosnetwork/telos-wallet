6ba6d6792b9d4767310a51dc02af7ac595087f05
Don Peat <don@caleos.io>
2023-06-05 15:19:57 -0500

    feat: trigger auto login if already connected on click connect WC

--- src/pages/home/ConnectWalletOptions.vue
+++ src/pages/home/ConnectWalletOptions.vue
@@ -1,7 +1,7 @@
 
 
 <script lang="ts">
-import { computed, defineComponent, inject, ref, watch } from 'vue';
+import { ComponentInternalInstance, computed, defineComponent, getCurrentInstance, inject, ref, watch } from 'vue';
 import { Web3Modal } from '@web3modal/html';
 import { EthereumClient } from '@web3modal/ethereum';
 import { useEVMStore, usePlatformStore, useAccountStore, useChainStore } from 'src/antelope';
@@ -16,13 +16,14 @@ export default defineComponent({
         },
     },
     setup(props){
+        const globalProps = (getCurrentInstance() as ComponentInternalInstance).appContext.config.globalProperties;
         const wagmiClient = inject('$wagmi') as EthereumClient;
         const web3Modal = ref<Web3Modal>();
         const supportsMetamask = computed(() => useEVMStore().isMetamaskSupported);
 
-        watch(() => props.toggleWalletConnect, (newVal) => {
+        watch(() => props.toggleWalletConnect, async (newVal) => {
             if (newVal) {
-                connectToWalletConnect();
+                await toggleWalletConnectModal();
             };
         });
 
@@ -33,9 +34,28 @@ export default defineComponent({
             accountStore.loginEVM({ network });
         };
 
+        const toggleWalletConnectModal = async () => {
+            // if already connected, trigger autologin
+            if (localStorage.getItem('wagmi.connected')){
+                await connectToWalletConnect();
+            }else {
+                await (web3Modal.value as Web3Modal).openModal();
+            }
+        };
+
         const connectToWalletConnect = async () => {
-            if(web3Modal.value) {
-                await web3Modal.value.openModal();
+            globalProps.$emitter.emit('toggleWalletConnect');
+
+            loginEvm();
+
+            const chainSettings = useChainStore().currentChain.settings;
+            const appChainId = chainSettings.getChainId();
+            const networkName = chainSettings.getDisplay();
+            const walletConnectChainId = getNetwork().chain?.id.toString();
+
+            if (appChainId !== walletConnectChainId){
+                const warningMessage = globalProps.$t('evm_wallet.incorrect_network', { networkName });;
+                globalProps.$warningNotification(warningMessage);
             }
         };
 
@@ -65,20 +85,7 @@ export default defineComponent({
 
         this.web3Modal.subscribeModal(async (newState) => {
             if (newState.open === false) {
-                this.$emit('toggleWalletConnect');
-                if (localStorage.getItem('wagmi.connected')){
-                    this.loginEvm();
 
-                    const chainSettings = useChainStore().currentChain.settings;
-                    const appChainId = chainSettings.getChainId();
-                    const networkName = chainSettings.getDisplay();
-                    const walletConnectChainId = getNetwork().chain?.id.toString();
-
-                    if (appChainId !== walletConnectChainId){
-                        const warningMessage = this.$t('evm_wallet.incorrect_network', { networkName });;
-                        (this as any).$warningNotification(warningMessage);
-                    }
-                }
             }
         });
     },
