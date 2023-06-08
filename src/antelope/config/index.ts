8a288bf0bc11417aa6cbb5cc4f56e2d4da7439b9
Viterbo <viter.rod@gmail.com>
2023-06-08 16:00:48 -0300

    accing comments explaining time units

--- src/antelope/config/index.ts
+++ src/antelope/config/index.ts
@@ -4,10 +4,10 @@ import { getAntelope } from '..';
 
 export class AntelopeConfig {
     // indexer health threshold --
-    private __indexer_health_threshold = 10;
+    private __indexer_health_threshold = 10; // 10 seconds
 
     // indexer health check interval --
-    private __indexer_health_check_interval = 1000 * 60 * 5;
+    private __indexer_health_check_interval = 1000 * 60 * 5; // 5 minutes expressed in milliseconds
 
     // notifucation handlers --
     private __notify_error_handler: (message: string) => void = m => alert(`Error: ${m}`);
@@ -72,7 +72,7 @@ export class AntelopeConfig {
         return this.__app;
     }
 
-    get indexerHealthThreshold() {
+    get indexerHealthThresholdSeconds() {
         return this.__indexer_health_threshold;
     }
 
