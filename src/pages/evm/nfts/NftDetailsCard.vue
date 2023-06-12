7b3816bc1e6b84d5ca5df9ffb4405ec9dbca3e22
Ezra Sowden-Guzman <ezratic@gmail.com>
2023-06-12 16:43:59 -0400

    add card component file

---
+++ src/pages/evm/nfts/NftDetailsCard.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import ExternalLink from 'components/ExternalLink.vue';
+
+const props = defineProps<{
+    title: string,
+}>();
+</script>
+
+<template>
+<div class="c-nft-details-card">
+    <h5 class="q-mb-sm">{{ title }}</h5>
+
+    <slot></slot>
+</div>
+</template>
+
+<style lang="scss">
+
+.c-nft-details-card {
+    border-radius: 4px;
+    border: 1px solid darken($page-header, 5%);
+    background-color: var(--bg-color);
+    padding: 16px;
+}
+</style>
