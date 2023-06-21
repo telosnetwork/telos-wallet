e601992618f6b749c6e2633958270b0c98e6ab0b
Don Peat <don@caleos.io>
2023-06-21 17:25:46 -0500

    fix: restore error message to switch to correct chain on mobile
    refactor: chain id check for walletconnect

--- src/antelope/stores/utils/checkNetwork.ts
+++ src/antelope/stores/utils/checkNetwork.ts
@@ -1,5 +1,6 @@
+import { getNetwork } from '@wagmi/core';
 import { ethers } from 'ethers';
-import { useEVMStore, usePlatformStore } from 'src/antelope';
+import { useChainStore, useEVMStore, usePlatformStore } from 'src/antelope';
 
 export const checkNetwork = async (): Promise<ethers.providers.Web3Provider | void> => {
     if (usePlatformStore().isMobile){
@@ -9,4 +10,11 @@ export const checkNetwork = async (): Promise<ethers.providers.Web3Provider | vo
     let webProvider = new ethers.providers.Web3Provider(provider);
     webProvider = await useEVMStore().ensureCorrectChain(webProvider);
     return webProvider;
+};
+
+export const isCorrectNetwork = (): boolean => {
+    const chainSettings = useChainStore().currentChain.settings;
+    const appChainId = chainSettings.getChainId();
+    const walletConnectChainId = getNetwork().chain?.id.toString();
+    return appChainId === walletConnectChainId;
 };
