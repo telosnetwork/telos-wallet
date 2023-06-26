e1fe2cfb260aa11e1c6ae97eea5b25e659db6c29
Ezra Sowden-Guzman <ezratic@gmail.com>
2023-06-26 14:18:25 -0400

    update table control styling for disabled buttons

--- src/components/evm/TableControls.vue
+++ src/components/evm/TableControls.vue
@@ -93,7 +93,7 @@ function changePageNumber(direction: 'next' | 'prev' | 'first' | 'last') {
         <q-btn
             outline
             size="sm"
-            color="primary"
+            :color="pagination.page === 1 ? 'gray-4' : 'primary'"
             icon="first_page"
             class="q-mr-xs"
             :disable="pagination.page === 1"
@@ -102,7 +102,7 @@ function changePageNumber(direction: 'next' | 'prev' | 'first' | 'last') {
         <q-btn
             outline
             size="sm"
-            color="primary"
+            :color="pagination.page === 1 ? 'gray-4' : 'primary'"
             icon="chevron_left"
             class="q-mr-sm"
             :disable="pagination.page === 1"
@@ -114,7 +114,7 @@ function changePageNumber(direction: 'next' | 'prev' | 'first' | 'last') {
         <q-btn
             outline
             size="sm"
-            color="primary"
+            :color="pagination.page === totalPages ? 'gray-4' : 'primary'"
             icon="chevron_right"
             class="q-ml-sm"
             :disable="pagination.page === totalPages"
@@ -123,7 +123,7 @@ function changePageNumber(direction: 'next' | 'prev' | 'first' | 'last') {
         <q-btn
             outline
             size="sm"
-            color="primary"
+            :color="pagination.page === totalPages ? 'gray-4' : 'primary'"
             icon="last_page"
             class="q-ml-xs"
             :disable="pagination.page === totalPages"
