138ec46cb7b0cf9f7228e19df74bfe6895eb7e85
Ezra Sowden-Guzman <ezratic@gmail.com>
2023-07-05 16:03:43 -0400

    fix prettyprintcurrency edge case, fix wrong header fiat value

--- src/pages/evm/wrap/WrapPageHeader.vue
+++ src/pages/evm/wrap/WrapPageHeader.vue
@@ -1,30 +1,37 @@
 <script setup lang="ts">
-import { computed } from 'vue';
+import { computed, onBeforeMount, ref } from 'vue';
 import { useUserStore, useBalancesStore, useChainStore } from 'src/antelope';
-import { getCurrencySymbol } from 'src/antelope/stores/utils/currency-utils';
+import { prettyPrintCurrency } from 'src/antelope/stores/utils/currency-utils';
 import { NativeCurrencyAddress } from 'src/antelope/types';
 import EVMChainSettings from 'src/antelope/chains/EVMChainSettings';
 import { WEI_PRECISION, formatWei } from 'src/antelope/stores/utils';
+import { useI18n } from 'vue-i18n';
 
+const { t: $t } = useI18n();
 const userStore = useUserStore();
 const balancesStore = useBalancesStore();
 const chainStore = useChainStore();
 const chainSettings = (chainStore.loggedChain.settings as EVMChainSettings);
 
-// eztodo loading state
+// data
+const systemTokenPrice = ref(0); // always equal to wrapped token price
+const loading = ref(true);
+
 // computed
 const fiatLocale = computed(() => userStore.fiatLocale);
 const fiatCurrency = computed(() => userStore.fiatCurrency);
-const fiatSymbol = computed(() => getCurrencySymbol(fiatLocale.value, fiatCurrency.value));
 const totalFiatValueText = computed(() => {
-    const systemTokenFiatValueBn = chainSettings.getSystemToken().price.value;
-    const wrappedTokenFiatValueBn = chainSettings.getWrappedSystemToken().price.value;
-    const added = systemTokenFiatValueBn.add(wrappedTokenFiatValueBn);
+    if (loading.value) {
+        return '--';
+    }
 
-    // eztodo this is 0.0 when value is 0, should be 0.00
-    return `${fiatSymbol.value} ${formatWei(added, WEI_PRECISION)}`;
+    const totalBalance = +totalSystemAndWrappedBalance.value;
+    const totalFiatValue = totalBalance * systemTokenPrice.value;
+
+    return prettyPrintCurrency(totalFiatValue, 2, fiatLocale.value, false, fiatCurrency.value, false);
 });
 
+const systemTokenName = computed(() => chainSettings.getSystemToken().name);
 const allBalances = computed(() => balancesStore.loggedBalances);
 const systemTokenBalanceBn =  computed(() => allBalances.value.find(b => b.token.contract === NativeCurrencyAddress)?.balance);
 const wrappedTokenBalanceBn = computed(() =>
@@ -44,21 +51,27 @@ const totalBalanceUnitsText = computed(() => {
 
     return `${systemSymbol} + ${wrappedSymbol}`;
 });
+
+
+// methods
+onBeforeMount(() => {
+    chainSettings.getUsdPrice().then((price) => {
+        systemTokenPrice.value = price;
+    }).finally(() => {
+        loading.value = false;
+    });
+});
 </script>
 
 <template>
-<div class="c-wrap-header">
-    <!-- eztodo i18n -->
-    <h5>Total of Wrapped Telos and Telos</h5>
-    <h1>{{ totalFiatValueText }}</h1>
-    <p class="o-text--small-bold u-text--low-contrast">
+<div class="text-center">
+    <h5>{{ $t('evm_wrap.total_of_wrapped_and_unwrapped', { token: systemTokenName }) }}</h5>
+    <h1 class="u-text--high-contrast">{{ totalFiatValueText }}</h1>
+    <p v-if="!loading" class="o-text--small-bold u-text--low-contrast">
         {{ totalSystemAndWrappedBalance }} {{ totalBalanceUnitsText }}
     </p>
 </div>
 </template>
 
 <style lang="scss">
-.c-wrap-header {
-    text-align: center;
-}
 </style>
