a7c2c2bc144a229478cf63d6b4d21e180f952289
Viterbo <viter.rod@gmail.com>
2023-07-13 15:12:15 -0300

    back to http

--- quasar.conf.js
+++ quasar.conf.js
@@ -97,7 +97,7 @@ module.exports = function(/* ctx */) {
 
         // Full list of options: https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-devServer
         devServer: {
-            https: true,
+            https: false,
             port: 8081,
             open: true, // opens browser window automatically
         },
