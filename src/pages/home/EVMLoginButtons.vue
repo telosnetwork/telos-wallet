02f2769b584312c881d464b125fc38d0691a2253
Viterbo <viter.rod@gmail.com>
2023-07-25 22:30:24 -0300

    adding alert

--- src/pages/home/EVMLoginButtons.vue
+++ src/pages/home/EVMLoginButtons.vue
@@ -13,18 +13,20 @@ export default defineComponent({
         const injected = ref(useEVMStore().injectedProviderNames.length);
         const isMobile = ref(usePlatformStore().isMobile);
 
+        alert('Hello from EVMLoginButtons.vue');
+
         const toggleWalletOptions = () => {
             if (isMobile.value) {
                 if (injected.value === 1) {
                     console.assert(useEVMStore().injectedProviderNames.length === 1, 'only one injected provider is supported for mobile');
-                    alert('useInjectedProvider');
+                    alert(`useInjectedProvider injected:${injected.value} isMobile:${isMobile.value}`);
                     emit('useInjectedProvider');
                 } else {
-                    alert('showWalletConnect');
+                    alert(`showWalletConnect injected:${injected.value} isMobile:${isMobile.value}`);
                     emit('showWalletConnect');
                 }
             } else {
-                alert('showWalletOptions');
+                alert(`showWalletOptions injected:${injected.value} isMobile:${isMobile.value}`);
                 emit('showWalletOptions');
             }
         };
