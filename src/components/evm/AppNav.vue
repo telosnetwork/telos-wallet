64067da552b206124de5b9dc1cbd10f416c29c16
Ezra Sowden-Guzman <ezratic@gmail.com>
2023-08-18 17:10:04 -0400

    use i18n for apy label

--- src/components/evm/AppNav.vue
+++ src/components/evm/AppNav.vue
@@ -279,7 +279,7 @@ export default defineComponent({
                     aria-hidden="true"
                 />
                 {{ $t('nav.staking') }}
-                <span class="c-app-nav__apy-box"> APY <b>8.23%</b> </span>
+                <span class="c-app-nav__apy-box">  {{ $t('evm_stake.apy_card_label') }} <b>8.23%</b> </span>
             </li>
 
             <li
