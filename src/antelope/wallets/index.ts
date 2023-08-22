a5066f4ff8eb29dfe006661315147295f1507a6f
Viterbo <viter.rod@gmail.com>
2023-08-22 16:38:41 -0300

    saving WIP

--- src/antelope/wallets/index.ts
+++ src/antelope/wallets/index.ts
@@ -26,12 +26,12 @@ export class AntelopeWallets {
         this.trace('getAutenticator', name);
         return this.authenticators.get(name);
     }
+
 }
 
 export * from 'src/antelope/wallets/authenticators/EVMAuthenticator';
 export * from 'src/antelope/wallets/authenticators/InjectedProviderAuth';
 export * from 'src/antelope/wallets/authenticators/MetamaskAuth';
 export * from 'src/antelope/wallets/authenticators/OreIdAuth';
-export * from 'src/antelope/wallets/authenticators/SafePalAuth';
 export * from 'src/antelope/wallets/authenticators/WalletConnectAuth';
 export * from 'src/antelope/wallets/authenticators/SafePalAuth';
