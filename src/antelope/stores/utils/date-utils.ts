53cc7950907f1cd58450982750854edb277ca1d4
Ezra Sowden-Guzman <ezratic@gmail.com>
2023-08-23 15:23:53 -0400

    ensure stale prices are not shown

---
+++ src/antelope/stores/utils/date-utils.ts
@@ -0,0 +1,12 @@
+export function dateIsWithinXMinutes(epochMs: number, minutes: number) {
+    if (epochMs <= 0) {
+        throw new Error('epochMs must be greater than 0');
+    }
+
+    // make a date object which represents the time X minutes ago
+    const xMinsAgo = new Date();
+    xMinsAgo.setMinutes(xMinsAgo.getMinutes() - minutes);
+
+    // return true if the date is within the defined timeframe
+    return new Date(epochMs) > xMinsAgo;
+}
