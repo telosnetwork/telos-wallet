2476a731042072853747d8a1414f1f74d9857fa5
Ezra Sowden-Guzman <ezratic@gmail.com>
2023-10-13 16:45:25 -0400

    add erc721 preview

--- src/pages/evm/allowances/AllowancesTableRow.vue
+++ src/pages/evm/allowances/AllowancesTableRow.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { computed } from 'vue';
+import { computed, onMounted, ref, watch } from 'vue';
 
 import {
     ShapedAllowanceRow,
@@ -11,8 +11,12 @@ import {
     isNftCollectionAllowanceRow,
 } from 'src/antelope/types/Allowances';
 import { prettyPrintCurrency } from 'src/antelope/stores/utils/currency-utils';
-import { useUserStore } from 'src/antelope';
+import { CURRENT_CONTEXT, useAccountStore, useNftsStore, useUserStore } from 'src/antelope';
 import { truncateAddress, truncateText } from 'src/antelope/stores/utils/text-utils';
+
+import ToolTip from 'src/components/ToolTip.vue';
+import NftViewer from 'pages/evm/nfts/NftViewer.vue';
+import { NFTClass } from 'src/antelope/types';
 
 const tlosLogo = require('src/assets/logo--tlos.svg');
 
@@ -20,18 +24,23 @@ const props = defineProps<{
     row: ShapedAllowanceRow;
 }>();
 
+const accountStore = useAccountStore();
+const nftStore = useNftsStore();
 const fiatLocale = useUserStore().fiatLocale;
 
 const isErc20Row = isErc20AllowanceRow(props.row);
 const isSingleErc721Row = isErc721SingleAllowanceRow(props.row);
 const isCollectionRow = isNftCollectionAllowanceRow(props.row);
 
+// data
+const erc721Nft = ref<NFTClass | null>(null);
+
 // computed
 const rowAsErc20Row = computed(() => props.row as ShapedAllowanceRowERC20);
 const rowAsSingleErc721Row = computed(() => props.row as ShapedAllowanceRowSingleERC721);
 const rowAsCollectionRow = computed(() => props.row as ShapedAllowanceRowNftCollection);
 
-const assetText = computed(() => {
+const assetTextShort = computed(() => {
     let prettyAmount: string;
 
     if (isSingleErc721Row) {
@@ -48,11 +57,35 @@ const assetText = computed(() => {
     return prettyAmount;
 });
 
+const assetTextFull = computed(() => {
+    let prettyAmount: string;
+
+    if (isSingleErc721Row) {
+        prettyAmount = '1 '.concat(props.row.tokenName);
+    } else if (isErc20Row) {
+        const { balance, tokenSymbol, tokenDecimals } = props.row;
+        prettyAmount = prettyPrintCurrency(balance, 4, fiatLocale, false, tokenSymbol, false, tokenDecimals, false);
+    } else {
+        const { balance, collectionName, collectionAddress } = props.row;
+        const collectionNamePretty = collectionName ?? collectionAddress;
+        prettyAmount = prettyPrintCurrency(balance, 0, fiatLocale, false, collectionNamePretty, false, 0, false);
+    }
+
+    return prettyAmount;
+});
+
+// methods
+onMounted(async () => {
+    if (isSingleErc721Row) {
+        const { collectionAddress, tokenId } = rowAsSingleErc721Row.value;
+        erc721Nft.value = await nftStore.fetchNftDetails(CURRENT_CONTEXT, collectionAddress, tokenId, 'ERC721');
+    }
+});
 </script>
 
 <template>
 <q-tr>
-    <q-td key="asset">
+    <q-td key="asset" class="c-allowances-table-row__asset-td">
         <img
             v-if="isErc20Row"
             :src="rowAsErc20Row.tokenLogo ?? tlosLogo"
@@ -64,10 +97,18 @@ const assetText = computed(() => {
                 'c-allowances-table-row__asset-logo--gray': !rowAsErc20Row.tokenLogo,
             }"
         >
-        <span class="o-text--paragraph-bold u-text--default-contrast">
-            {{ assetText }}
-        </span>
-        <!-- tooltip here -->
+        <NftViewer
+            v-else-if="erc721Nft && isSingleErc721Row"
+            :nft="erc721Nft"
+            :preview-mode="false"
+            :tile-mode="false"
+            :icon-size="24"
+        />
+        <ToolTip :text="assetTextFull" :hide-icon="true">
+            <span class="o-text--paragraph-bold u-text--default-contrast">
+                {{ assetTextShort }}
+            </span>
+        </ToolTip>
     </q-td>
     <q-td key="balance">
         {{ row.lastUpdated }}
@@ -89,6 +130,12 @@ const assetText = computed(() => {
 
 <style lang="scss">
 .c-allowances-table-row {
+    &__asset-td {
+        display: flex;
+        align-items: center;
+        gap: 8px;
+    }
+
     &__asset-logo {
         width: 24px;
         height: 24px;
--- src/pages/evm/nfts/NftViewer.vue
+++ src/pages/evm/nfts/NftViewer.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { computed, ref, onMounted } from 'vue';
+import { computed, ref, onMounted, watch } from 'vue';
 import { ShapedNFT } from 'src/antelope/types';
 import { useI18n } from 'vue-i18n';
 import { usePlatformStore } from 'src/antelope';
@@ -8,9 +8,10 @@ const platformStore = usePlatformStore();
 const { t: $t } = useI18n();
 
 const props = defineProps<{
-    nft: ShapedNFT,
-    previewMode: boolean, // controls whether video/audio can be played, and how those types are displayed
-    tileMode: boolean,
+    nft: ShapedNFT;
+    previewMode: boolean; // controls whether video/audio can be played, and how those types are displayed
+    tileMode: boolean;
+    iconSize?: number; // only used in icon mode
 }>();
 
 // data
@@ -96,13 +97,26 @@ const iconOverlayName = computed(() => {
     return 'o_image_not_supported';
 });
 
+// watchers
+watch(props, () => {
+    setIconSizeVariable();
+}, {
+    immediate: true,
+});
+
 onMounted(() => {
+    setIconSizeVariable();
+
     setTimeout(() => {
         passedMaxLoadingTime.value = true;
     }, 5000);
 });
 
 // methods
+function setIconSizeVariable() {
+    rootElement.value?.style.setProperty('--list-icon-size', `${props.iconSize ?? 40}px`);
+}
+
 function playVideo() {
     if (!isIos.value) {
         // prevent weird iOS behavior where video pauses and unpauses quickly
@@ -193,6 +207,14 @@ function setHoverPreviewVisibility(visible: boolean) {
     calculateHoverPreviewPosition();
     isHovering.value = visible;
 }
+
+function getIconSize(defaultSize: string) {
+    if (props.iconSize) {
+        return `${props.iconSize}px`;
+    }
+
+    return defaultSize;
+}
 </script>
 
 <template>
@@ -241,7 +263,7 @@ function setHoverPreviewVisibility(visible: boolean) {
                     v-if="passedMaxLoadingTime && isMediaLoading"
                     :alt="`${$t('nft.broken_image')} ${imageAlt}`"
                     name="o_broken_image"
-                    size="md"
+                    :size="getIconSize('md')"
                     color="grey-7"
                     class="c-nft-viewer__image"
                 />
@@ -285,7 +307,7 @@ function setHoverPreviewVisibility(visible: boolean) {
 
                 <q-icon
                     :name="iconOverlayName"
-                    size="lg"
+                    :size="getIconSize('lg')"
                     color="primary"
                     class="c-nft-viewer__overlay-icon"
                 />
@@ -311,7 +333,7 @@ function setHoverPreviewVisibility(visible: boolean) {
             v-else-if="nft.videoSrc"
             name="o_movie"
             :alt="imageAlt"
-            size="md"
+            :size="getIconSize('md')"
             color="grey-7"
             class="c-nft-viewer__list-image"
         />
@@ -319,7 +341,7 @@ function setHoverPreviewVisibility(visible: boolean) {
             v-else-if="(isMediaLoading && passedMaxLoadingTime) || (!nft.imageSrcFull)"
             name="o_broken_image"
             :alt="`${$t('nft.broken_image')} ${imageAlt}`"
-            size="md"
+            :size="getIconSize('md')"
             color="grey-7"
             class="c-nft-viewer__list-image"
         />
@@ -328,8 +350,8 @@ function setHoverPreviewVisibility(visible: boolean) {
             :src="nft.imageSrcFull"
             :alt="`${$t('nft.collectible')} ${imageAlt}`"
             class="c-nft-viewer__list-image"
-            height="40"
-            width="40"
+            :height="iconSize ?? 40"
+            :width="iconSize ?? 40"
             @load="isMediaLoading = false"
         >
     </div>
@@ -365,7 +387,7 @@ function setHoverPreviewVisibility(visible: boolean) {
                 v-if="passedMaxLoadingTime && isMediaLoading"
                 :alt="`${$t('nft.broken_image')} ${imageAlt}`"
                 name="o_broken_image"
-                size="md"
+                :size="getIconSize('md')"
                 color="grey-7"
                 class="c-nft-viewer__image"
             />
@@ -377,12 +399,11 @@ function setHoverPreviewVisibility(visible: boolean) {
 <style lang="scss">
 .c-nft-viewer {
     $this: &;
-    $list-icon-size: 40px;
     height: 100%;
 
     &--icon-mode {
-        height: $list-icon-size;
-        width: $list-icon-size;
+        height: var(--list-icon-size);
+        width: var(--list-icon-size);
     }
 
     &__nft-container {
