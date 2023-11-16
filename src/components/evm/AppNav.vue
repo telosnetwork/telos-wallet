8faafe44ab77b017aa3a36d8b617b14efc56030f
Viterbo <viter.rod@gmail.com>
2023-11-16 18:42:30 -0300

    changing hostname for origin

--- src/components/evm/AppNav.vue
+++ src/components/evm/AppNav.vue
@@ -56,7 +56,7 @@ export default defineComponent({
             return '0';
         },
         isProduction() {
-            return process.env.NODE_ENV === 'production' && !window.location.hostname.includes('netlify');
+            return process.env.NODE_ENV === 'production' && !window.location.origin.includes('netlify');
         },
         accountActionText() {
             if (this.loggedAccount) {
