20d8102fd73e655f164f9451ccd13086fe597ac2
Viterbo <viter.rod@gmail.com>
2023-11-25 12:28:02 -0300

    avoiding local storage gets cleared every session

--- src/App.vue
+++ src/App.vue
@@ -11,31 +11,33 @@ export default {
         const userIsNew = !localStorage.getItem('account');
         const currentVersion = packageInfo.version;
         const clientVersion = localStorage.getItem('appVersion');
-
         console.info('Wallet version: ', packageInfo.version);
 
-        if (clientVersion && clientVersion !== appVersionJustUpdated) {
-            console.info('Client version: ', clientVersion);
-        }
+        if (clientVersion !== currentVersion) {
+            if (clientVersion && clientVersion !== appVersionJustUpdated) {
+                console.info('Client version: ', clientVersion);
+            }
 
-        // when localstorage is cleared, we need to reload the page for it to take effect.
-        // however if we immediately reload the page here we cannot show a notification to the user.
-        // so the const appVersionUpdated lets us know after the reload that we just cleared the old localStorage
-        // and need to notify the user that they need to log in again
-        if (clientVersion === appVersionJustUpdated) {
-            console.info('App version mismatch, local storage cleared');
-            // App version was updated, localStorage was cleared, and the page reloaded
-            // Now inform the user that the app was updated & have them login again, and set the client app version
-            localStorage.setItem('appVersion', currentVersion);
-            (this as any).$notifySuccessMessage(
-                (this as any).$t('global.new_app_version'),
-            );
-        } else if (userIsNew) {
-            localStorage.setItem('appVersion', currentVersion);
-        } else if (clientVersion !== currentVersion) {
-            localStorage.clear();
-            localStorage.setItem('appVersion', appVersionJustUpdated);
-            window.location.reload();
+            // when localstorage is cleared, we need to reload the page for it to take effect.
+            // however if we immediately reload the page here we cannot show a notification to the user.
+            // so the const appVersionUpdated lets us know after the reload that we just cleared the old localStorage
+            // and need to notify the user that they need to log in again
+            if (clientVersion === appVersionJustUpdated) {
+                console.info('App version mismatch, local storage cleared');
+                // App version was updated, localStorage was cleared, and the page reloaded
+                // Now inform the user that the app was updated & have them login again, and set the client app version
+                localStorage.setItem('appVersion', currentVersion);
+                (this as any).$notifySuccessMessage(
+                    (this as any).$t('global.new_app_version'),
+                );
+            } else if (userIsNew) {
+                localStorage.clear();
+                localStorage.setItem('appVersion', currentVersion);
+            } else {
+                localStorage.clear();
+                localStorage.setItem('appVersion', appVersionJustUpdated);
+                window.location.reload();
+            }
         }
     },
     mounted() {
