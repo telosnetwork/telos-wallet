50a92a5ffd5a202ab26bee47488838af2aea4ad1
Ezra Sowden-Guzman <ezratic@gmail.com>
2023-11-30 17:52:57 -0500

    fix supported interfaces issue

--- src/antelope/chains/EVMChainSettings.ts
+++ src/antelope/chains/EVMChainSettings.ts
@@ -386,8 +386,7 @@ export default abstract class EVMChainSettings implements ChainSettings {
 
         // we fix the supportedInterfaces property if it is undefined in the response but present in the request
         Object.values(response.contracts).forEach((contract) => {
-            contract.supportedInterfaces = contract.supportedInterfaces ||
-                params.type ? [params.type?.toLowerCase() as string] : undefined;
+            contract.supportedInterfaces = contract.supportedInterfaces || (params.type ? [params.type.toLowerCase()] : undefined);
         });
 
         this.processNftContractsCalldata(response.contracts);
