4b0cacbe5664342f7cb7221ca7014febd14a08c6
Ezra Sowden-Guzman <ezratic@gmail.com>
2023-12-18 14:22:59 -0500

    cleanup

--- src/pages/evm/allowances/EditAllowanceModal.vue
+++ src/pages/evm/allowances/EditAllowanceModal.vue
@@ -1,5 +1,10 @@
 <script setup lang="ts">
-import { computed, onBeforeMount, ref, watch } from 'vue';
+import {
+    computed,
+    onBeforeMount,
+    ref,
+    watch,
+} from 'vue';
 import { useI18n } from 'vue-i18n';
 import { BigNumber } from 'ethers';
 
