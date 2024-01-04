b920fd3a862fe8afa88e06d898d9a78607765153
Ezra Sowden-Guzman <ezratic@gmail.com>
2024-01-04 18:29:36 -0500

    update links

--- src/antelope/chains/evm/telos-evm-testnet/index.ts
+++ src/antelope/chains/evm/telos-evm-testnet/index.ts
@@ -149,7 +149,7 @@ export default class TelosEVMTestnet extends EVMChainSettings {
     }
 
     getBuyMoreOfTokenLink(): string {
-        return 'https://www.telos.net/#buy-tlos-simplex';
+        return 'https://telos.net/ecosystem?category=Exchanges';
     }
 
     getSystemTokens(): TokenClass[] {
