6398a0a22a90737fcafbc58beb888984aadecfb8
Ezra Sowden-Guzman <ezratic@gmail.com>
2024-01-15 16:48:07 -0500

    add event logging, make new instance on show widget

--- src/pages/demo/RampDemo.vue
+++ src/pages/demo/RampDemo.vue
@@ -1,15 +1,17 @@
 <script setup lang="ts">
 import { RampInstantSDK } from '@ramp-network/ramp-instant-sdk';
 
-const rampSdk = new RampInstantSDK({
+const widgetParams = {
     hostAppName: 'Telos Demo',
     hostLogoUrl: 'https://raw.githubusercontent.com/telosnetwork/token-list/main/logos/telos.png',
     hostApiKey: '55wtuyuew2zyawyxnw2gv6aubuypn4vq7mbwfxz9',
     url: 'https://app.demo.ramp.network',
-});
+};
 
 function showRampSdk() {
-    rampSdk.show();
+    new RampInstantSDK(widgetParams).on('*', (event: unknown) => {
+        console.log(event);
+    }).show();
 }
 </script>
 
